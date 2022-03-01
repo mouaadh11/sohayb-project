a61b0ffe4a92f3fedef537259ba702eb209bc293
mouaadh sahailia <99537594+mouaadh11@users.noreply.github.com>
2022-03-01 20:10:51 +0100

    start

---
+++ menu.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+
+     printf("\n\n\n\t\t\tMAIN MENU\n\t\t=====================\n\t\t[1] Add a new Contact\n\t\t[2] List all Contacts\n\t\t[3] Search for contact\n\t\t[4] Edit a Contact\n\t\t[5] Delete a Contact\n\t\t[0] Exit\n\t\t=================\n\t\t");
+     return 0;
+}
